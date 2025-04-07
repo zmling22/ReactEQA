6fad7ce1681ae9f5082203c63bdfec3c8daa1b2e
zmling22 <zmling22@gmail.com>
2025-04-07 14:49:38 +0800

    init

---
+++ data/ToolTrajectory/generator_deerapi.py
@@ -0,0 +1,122 @@
+import os
+from openai import OpenAI
+import base64
+import csv
+from tqdm import tqdm
+import http.client
+import json
+
+def convert_image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+def requests_api(images, prompt):
+    image_urls = []
+    for image in images:
+        base64_image = convert_image_to_base64(image)
+        image_urls.append({"type": "image_url", "image_url": {"url": f"data:image/png;base64,{base64_image}"}})
+    prompt = [{"type": "text", "text": prompt}]
+    content = prompt + image_urls
+
+    conn = http.client.HTTPSConnection('api.deerapi.com')
+    payload = json.dumps({
+        "model": "gpt-4o-mini",
+        "stream": False,
+        "messages": [
+            {
+                "role": "user",
+                "content": content
+            }
+            ],
+            "max_tokens": 400
+        })
+    headers = {
+        'Authorization': 'sk-lrenmYBYEOQH0rqv9rlMmoTaELkvZni1afswhr6be3tTN44S',
+        'Content-Type': 'application/json'
+    }
+    conn.request("POST", "/v1/chat/completions", payload, headers)
+    res = conn.getresponse()
+    data = json.loads(res.read().decode("utf-8"))
+
+    return data
+
+def post_process(result):
+    result = result.strip("`\n")
+    split = result.split("\n")
+    print(split)
+
+    question = split[0][10:]
+    options = split[1][9:].strip("[]").split("; ")
+    options = [option[2:].strip() for option in options]
+    answer = split[2][8:].strip("[]")[0]
+    label = split[3][7:].strip("[]")
+
+    # "scene", "floor", "question", "choices", "question_formatted", "answer", "label"
+    response = {
+        "floor": 0,
+        "question": question,
+        "choices": options,
+        "question_formatted": f"{question} A) {options[0]} B) {options[1]} C) {options[2]} D) {options[3]}. Answer:",
+        "answer": answer,
+        "label": label,
+    }
+    return response
+
+def save_csv(csv_file_path, csv_columns, generated_data):
+    with open(csv_file_path, "w", newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for data in generated_data:
+            writer.writerow(data)
+
+if __name__ == "__main__":
+    scene_root = "data/ReactEQA/sample_scene/images"
+    prompt_file = "data/ReactEQA/prompt_single.txt"
+
+    csv_columns = ["scene", "floor", "question", "choices", "question_formatted", "answer", "label", "source_image"]
+
+    generated_data = []
+    index = 0
+    scene_dir = os.listdir(scene_root)
+    scene_dir.sort()
+    
+    csv_file_path = 'data/ReactEQA/question.csv'
+
+    finished_samples = []
+    if os.path.exists(csv_file_path):
+        with open(csv_file_path, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                finished_samples.append(row['source_image'])
+
+    csvfile = open(csv_file_path, "a", newline='')
+    writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+
+    with open(prompt_file, "r") as file:
+        prompt = file.read()
+    
+    for scene in tqdm(scene_dir):
+        if index > 100:
+            break
+
+        index += 1
+        images_file = os.listdir(os.path.join(scene_root, scene))
+        images_file.sort()
+        images_file = [os.path.join(scene_root, scene, file) for file in images_file]
+        
+        result = requests_api(images_file, prompt)
+        result_dict = post_process(result["choices"][0]["message"]["content"])
+        result_dict["source_image"] = file
+        result_dict["scene"] = file.split("_")[0]
+        generated_data.append(result_dict)
+
+        # try:
+        #     result = requests_api(image_file, prompt)
+        #     result_dict = post_process(result["choices"][0]["message"]["content"])
+        #     result_dict["source_image"] = file
+        #     result_dict["scene"] = file.split("_")[0]
+        #     generated_data.append(result_dict)
+        # except:
+        #     continue
+
+        writer.writerow(result_dict)
---
+++ data/ToolTrajectory/sampler.py
@@ -0,0 +1,122 @@
+import habitat_sim
+import numpy as np
+import random
+import os
+import cv2
+import math
+import json
+import csv
+
+from tqdm import tqdm
+from src.habitat import (
+    make_simple_cfg,
+    pos_normal_to_habitat,
+    pos_habitat_to_normal,
+    pose_habitat_to_normal,
+    pose_normal_to_tsdf,
+)
+from habitat_sim.utils.common import quat_from_angle_axis
+
+# 确保场景数据路径正确
+scene_dir = "data/HM3D/"
+
+def create_sim(scene):
+    scene_file = os.path.join(
+            scene_dir, scene, scene[6:] + ".basis" + ".glb"
+        )
+    navmesh_file = os.path.join(
+            scene_dir, scene, scene[6:] + ".basis" + ".navmesh"
+        )
+    
+    # 配置模拟器参数
+    sim_settings = {
+            "scene": scene_file,
+            "default_agent": 0,
+            "sensor_height": 1.5,
+            "width": 640,
+            "height": 480,
+            "hfov": 120,
+    }
+    sim_cfg = make_simple_cfg(sim_settings)
+    sim = habitat_sim.Simulator(sim_cfg)
+
+    pathfinder = sim.pathfinder
+    pathfinder.load_nav_mesh(navmesh_file)
+
+    agent = sim.initialize_agent(sim_settings["default_agent"])
+    
+    return sim, agent
+
+def get_random_image(sim):
+    # 随机采样可行走点
+    target_position = sim.pathfinder.get_random_navigable_point()
+    
+    # 随机旋转（水平方向）
+    angle = np.random.uniform(0, 2 * np.pi)
+    rotation = quat_from_angle_axis(angle, np.array([0, 1, 0]))  # Y轴旋转
+    
+    # 设置 Agent 状态
+    agent_state = habitat_sim.AgentState()
+    agent_state.position = target_position
+    agent_state.rotation = rotation
+    agent.set_state(agent_state)
+    
+    # 获取观察结果
+    observation = sim.get_sensor_observations()
+    rgb_image = observation["color_sensor"]  # 形状 [H, W, 3]
+    
+    return rgb_image, agent_state
+
+def get_random_samples(sim, num_samples=3):
+    images = []
+    states = []
+    for _ in range(num_samples):
+        rgb_image, agent_state = get_random_image(sim)
+        images.append(rgb_image)
+        states.append(agent_state)
+
+    return images, states
+
+if __name__ == "__main__":
+    csv_columns = ["scene", "image_path", "position", "rotation"]    
+    csv_file_path = 'data/ReactEQA/sample_scene/position.csv'
+
+    finished_samples = []
+    if os.path.exists(csv_file_path):
+        with open(csv_file_path, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                finished_samples.append(row['source_image'])
+
+    csvfile = open(csv_file_path, "a", newline='')
+    writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+
+    for scene in tqdm(os.listdir(scene_dir)):
+        if not os.path.isdir(os.path.join(scene_dir, scene)):
+            continue
+        sim, agent = create_sim(scene)
+        sampled_images, sampled_states = get_random_samples(sim, 10)
+
+        datas = []
+        for i, (img, state) in enumerate(zip(sampled_images, sampled_states)):
+            # 保存图片
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            save_root = f"data/ReactEQA/sample_scene/images/{scene}"
+            if not os.path.exists(save_root):
+                os.makedirs(save_root)
+            image_path = os.path.join(save_root, f"{i}.png")
+            cv2.imwrite(image_path, img)
+
+            # 保存状态
+            pos = state.position
+            rot = state.rotation
+            rot = [rot.x, rot.y, rot.z, rot.w]
+            print(f"Scene: {scene}, Position: {pos}, Rotation: {rot}")
+            item = {"scene": scene, "image_path": image_path, "position": pos.tolist(), "rotation": rot}
+            datas.append(item)
+            writer.writerow(item)
+        try:
+            sim.close()
+        except:
+            pass
+    json.dump(datas, open("data/ReactEQA/sample_scene/position.json", "w"), indent=4)
